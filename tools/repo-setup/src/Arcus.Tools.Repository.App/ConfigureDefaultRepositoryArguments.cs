03dd0821becd3780a385d4035d20d228efaaf1ac
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-09-14 15:26:17 +0200

    WIP
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ tools/repo-setup/src/Arcus.Tools.Repository.App/ConfigureDefaultRepositoryArguments.cs
@@ -0,0 +1,19 @@
+﻿using CommandLineParser.Arguments;
+
+namespace Arcus.Tools.Repository.App
+{
+    public class ConfigureDefaultRepositoryArguments
+    {
+        [SwitchArgument(shortName: 'c', longName: "configure-default", defaultValue:true, Description = "Configures the GitHub repository according to our default standards")]
+        public bool ConfigureDefaults { get; set; }
+
+        [ValueArgument(typeof(string), 'u', "username",  Description = "Name to authenticate with", Optional = false)]
+        public string UserName { get; set; }
+
+        [ValueArgument(typeof(string), 'p', "password", Description = "Password to authenticate with", Optional = false)]
+        public string Password { get; set; }
+
+        [ValueArgument(typeof(string), 'r', "repo-name",  Description = "Name of the repository to configure", Optional = false)]
+        public string Repository { get; set; }
+    }
+}
---
+++ tools/repo-setup/src/Arcus.Tools.Repository.App/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Arcus.Tools.Repository.App
+{
+    public class Program
+    {
+        private const ConsoleColor AnnouncementColor = ConsoleColor.DarkYellow;
+
+        private const string ApplicationIntroduction = @"
+ █████╗ ██████╗  ██████╗██╗   ██╗███████╗
+██╔══██╗██╔══██╗██╔════╝██║   ██║██╔════╝
+███████║██████╔╝██║     ██║   ██║███████╗
+██╔══██║██╔══██╗██║     ██║   ██║╚════██║
+██║  ██║██║  ██║╚██████╗╚██████╔╝███████║
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝";
+
+        private const ConsoleColor RegularColor = ConsoleColor.White;
+
+        public static async Task Main(string[] args)
+        {
+            WelcomeUser();
+            await RunApplication(args);
+            CloseApplication();
+        }
+
+        private static void CloseApplication()
+        {
+            Console.WriteLine(value: "Press any key to close the application...");
+            Console.ReadLine();
+        }
+
+        private static CommandLineParser.CommandLineParser ConfigureCommandLineParser(ConfigureDefaultsCommandLineArguments configureDefaultsCommandLineArguments, string[] args)
+        {
+            var cmdlineParser = new CommandLineParser.CommandLineParser
+            {
+                ShowUsageHeader = "Here is how you use the repository provisioning app:",
+                ShowUsageFooter = "For more information, see https://github.com/arcus-azure/arcus"
+            };
+
+            cmdlineParser.ExtractArgumentAttributes(configureDefaultsCommandLineArguments);
+            //cmdlineParser.Arguments.AddRange(commandLineArguments);
+            cmdlineParser.ParseCommandLine(args);
+
+            return cmdlineParser;
+        }
+
+        private static async Task ConfigureDefaults()
+        {
+        }
+
+        private static async Task RunApplication(string[] args)
+        {
+            var configureDefaultsArguments = new ConfigureDefaultsCommandLineArguments();
+            //var configureDefaultsArgument = new SwitchArgument(shortName: 'c', longName: "configure-default", description: "Configures the GitHub repository according to our default standards", defaultValue: true);
+            //var gitHubUserNameArgument = new ValueArgument<string>(shortName: 'u', longName: "username", description: "Name to authenticate with");
+            //var gitHubPasswordArgument = new ValueArgument<string>(shortName: 'p', longName: "password", description: "Password to authenticate with");
+            //var gitHubRepoArgument = new ValueArgument<string>(shortName: 'r', longName: "repo-name", description: "Name of the repository to configure");
+            //var commandLineArguments = new List<SwitchArgument>
+            //{
+            //    configureDefaultsArgument
+            //};
+
+            var cmdlineParser = ConfigureCommandLineParser(configureDefaultsArguments, args);
+            if (configureDefaultsArguments.ConfigureDefaults)
+            {
+                await ConfigureDefaults();
+            }
+            else
+            {
+                ShowUsage(cmdlineParser);
+            }
+        }
+
+        private static void ShowUsage(CommandLineParser.CommandLineParser cmdlineParser)
+        {
+            Console.ForegroundColor = AnnouncementColor;
+            cmdlineParser.ShowUsage();
+            Console.ForegroundColor = RegularColor;
+        }
+
+        private static void WelcomeUser()
+        {
+            Console.ForegroundColor = AnnouncementColor;
+            Console.WriteLine(ApplicationIntroduction);
+            Console.ForegroundColor = RegularColor;
+        }
+    }
+}

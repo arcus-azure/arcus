219180fed139ed426404427d4df695b3e8c483f6
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2026-01-23 11:40:47 +0100

    chore(docusaurus): initial docusaurus infra setup (#379)
    Since the main Arcus website is outdated in terms of supported
    repositories, use cases and blog posts, plus is using a different
    hosting system than our other Arcus repositories, I propose that we
    migrate to Docusaurus. This will streamline how we expose Codit
    information (Arcus, DevOps Practice, Invictus) so that the most amount
    of people can do changes. And limiting ourselves with possibly
    unnecessary custom 'Markdown > HTML' conversions that is currently being
    done here using TypeScript.
    
    This PR is now solely here to setup the initial infrastructure, it does
    not yet change the hosted website in any way.

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,78 @@
+const lightCodeTheme = require('./src/prism/light');
+const darkCodeTheme = require('./src/prism/dark');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Arcus',
+  url: 'https://arcus-azure.net/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'arcus-azure', // Usually your GitHub org/user name.
+  projectName: 'Arcus', // Usually your repo name.
+  themeConfig: {
+    image: 'img/arcus.jpg',
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'Arcus',
+        src: 'img/arcus.png',
+        srcDark: 'img/arcus_for_dark.png',
+      },
+      items: [
+        {
+          type: 'search',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/arcus-azure/arcus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Arcus Azure Github',
+              href: 'https://github.com/arcus-azure',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()}, Arcus - maintained by arcus-azure`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+      additionalLanguages: ['csharp', 'fsharp', 'diff', 'json', 'powershell'],
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          path: 'preview',
+          sidebarCollapsible: false,
+          // Please change this to your repo.
+          editUrl: 'https://github.com/arcus-azure/arcus/edit/main/docs',
+          includeCurrentVersion: process.env.CONTEXT !== 'production',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  stylesheets: [
+    'https://fonts.googleapis.com/css2?family=Bitter:wght@700&family=Inter:wght@400;500&display=swap'
+  ],
+};

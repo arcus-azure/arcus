0160a5600afb27e4b5d0b517df72acf560c4b8fa
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2026-02-12 06:12:55 +0100

    chore: remove local .docusaurus folder from commit

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\dev\\github.com\\arcus\\docs\\node_modules\\.pnpm\\infima@0.2.0-alpha.45\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\dev\\github.com\\arcus\\docs\\node_modules\\.pnpm\\@docusaurus+theme-classic@3_913803fe8eb844585c6edc271c99276c\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\dev\\github.com\\arcus\\docs\\node_modules\\.pnpm\\@docusaurus+theme-classic@3_913803fe8eb844585c6edc271c99276c\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\dev\\github.com\\arcus\\docs\\src\\css\\custom.css"),
-];
